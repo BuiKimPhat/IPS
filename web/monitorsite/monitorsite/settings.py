0b184575571f9558e25688ac06c109cbdbd37eac
BuiKimPhat <buikimphat42@gmail.com>
2023-05-04 04:17:06 +0000

    Update models

--- web/monitorsite/monitorsite/settings.py
+++ web/monitorsite/monitorsite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'monitorweb.apps.MonitorwebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- web/monitorsite/monitorweb/models.py
+++ web/monitorsite/monitorweb/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Agent(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=200)
+    ip = models.GenericIPAddressField()
+    health = models.URLField(max_length=200)
+    registered_at = models.DateTimeField(auto_now_add=True)
+
+class Alert(models.Model):
+    def __str__(self):
+        return self.message
+    agent = models.ForeignKey(Agent, on_delete=models.PROTECT)
+    message = models.CharField(max_length=500)
+    src = models.GenericIPAddressField()
+    srcp = models.PositiveIntegerField(null=True)
+    dst = models.GenericIPAddressField()
+    dstp = models.PositiveIntegerField()
+    protocol = models.CharField(max_length=10)
+    action = models.CharField(max_length=50)
