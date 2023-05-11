353a4e9aca5d4f92f9da6f1e13c2f09a5dcfed85
BuiKimPhat <buikimphat42@gmail.com>
2023-05-11 11:11:49 +0700

    Add login view

--- web/monitorsite/monitorweb/urls.py
+++ web/monitorsite/monitorweb/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "monitorweb"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login, name="login"),
     path("agents", views.agents, name="agents"),
     path("alerts", views.alerts, name="alerts"),
 ]
