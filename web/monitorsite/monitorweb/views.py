d41b43eb5fb9d4f51c4de76a2b809dbd6aba837a
BuiKimPhat <buikimphat42@gmail.com>
2023-05-11 16:36:44 +0700

    Add welcome user, title for sidebar icons, redirect login if user is authenticated

--- web/monitorsite/monitorweb/views.py
+++ web/monitorsite/monitorweb/views.py
@@ -3,20 +3,27 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout as logout_func, login as login_func
 
 def login(request):
+    # Check if user is authenticated
+    if request.user.is_authenticated:
+        return redirect("monitorweb:index")
+    # GET request to login page (no credentials)
     context = { "messages": None }
     if "username" not in request.POST or "password" not in request.POST:
         return render(request, "monitorweb/login.html", context)
+
+    # POST request to login page (no credentials)
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(request, username=username, password=password)
-
     if user is not None:
+        # Correct credentials
         login_func(request, user)
         if "next" in request.GET:
             return redirect(request.GET["next"])
         else:
             return redirect("monitorweb:index")
     else:
+        # Incorrect credentials
         context["message"] = "Invalid credentials!"
         return render(request, "monitorweb/login.html", context)
 
