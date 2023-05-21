4f62d14c802e69af986a78091969fa5341395e7c
BuiKimPhat <buikimphat42@gmail.com>
2023-05-21 18:00:07 +0700

    Terraform use spot instance, change to instance type t3.medium

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "ssh_http" {
 
 # Monitor EC2
 module "monitor_ec2" {
-  
+
   create_spot_instance = true
   spot_price           = "0.20"
   spot_type            = "persistent"
