9396c78b209c0a4f5e714740ea9e7c1f8f659ae3
BuiKimPhat <buikimphat42@gmail.com>
2023-06-09 14:43:58 +0700

    Done notification

--- web/monitorsite/monitorweb/static/monitorweb/notification.js
+++ web/monitorsite/monitorweb/static/monitorweb/notification.js
@@ -1,7 +1,34 @@
 // Desktop notification
-const notify = () => {
-    
+const notify_request = () => {
+    if (!window.Notification) {
+        console.log('Browser does not support notifications.');
+    } else {
+        // check if permission is already granted
+        if (Notification.permission !== 'granted') {
+            // request permission from user
+            Notification.requestPermission().then(function (p) {
+                if (p === 'granted') {
+                    console.log('Desktop notification permission granted.')
+                } else {
+                    console.log('User blocked notifications.');
+                }
+            }).catch(function (err) {
+                console.error(err);
+            });
+        }
+    }
 }
+
+const notify = (data) => {
+    data.alerts.forEach(alert => {
+        new Notification(alert.rule_class + " attack at agent " + alert.agent_name, {
+            body: "Rule triggered: " + alert.rule_name + ". Action: " +
+            (alert.is_denied ? "Denied. " : "Allowed. ") + alert.message
+        });
+    });
+}
+
+
 // Socket
 const dropdown_child = data => {
     let li = document.createElement('li');
@@ -14,8 +41,8 @@ const dropdown_child = data => {
     header.className = "m-0";
     let content = document.createElement('p');
     content.className = "m-0";
-    content.textContent = "Rule triggered: " + data.rule_name + ". Action: " + 
-                            (data.is_denied ? "Denied. " : "Allowed. ") + data.message;
+    content.textContent = "Rule triggered: " + data.rule_name + ". Action: " +
+        (data.is_denied ? "Denied. " : "Allowed. ") + data.message;
     div.appendChild(header);
     div.appendChild(content);
     a.appendChild(div)
@@ -43,8 +70,8 @@ const toast_child = data => {
     closeBtn.setAttribute("data-bs-dismiss", "toast");
     let body = document.createElement('div');
     body.className = 'toast-body';
-    body.textContent = "Rule triggered: " + data.rule_name + ". Action: " + 
-                        (data.is_denied ? "Denied. " : "Allowed. ") + data.message;
+    body.textContent = "Rule triggered: " + data.rule_name + ". Action: " +
+        (data.is_denied ? "Denied. " : "Allowed. ") + data.message;
 
     header.appendChild(icon);
     header.appendChild(title);
@@ -60,18 +87,18 @@ const toast_child = data => {
 const update_dropdown_list = (data, max_noti) => {
     const noti_dropdown = document.getElementById("alert-notification-dropdown");
     const exist_alerts = Array.from(document.getElementsByClassName('dropdown-item')).map(item => item.parentElement.cloneNode(true));
-    
+
     const nodes = data.alerts.map(item => dropdown_child(item));
     // Concat if there are existing alerts
     let tmp = nodes.concat(exist_alerts);
     // Delete existing alerts, don't display the placeholder
     if (exist_alerts.length > 0) {
-        for (let i=0; i < exist_alerts.length; i++){
+        for (let i = 0; i < exist_alerts.length; i++) {
             noti_dropdown.removeChild(noti_dropdown.children[1])
-        }    
+        }
     } else noti_dropdown.removeChild(noti_dropdown.children[1]); // Remove "No alert" placeholder
     // Append latest alerts
-    for (let i=0; i < (max_noti <= tmp.length ? max_noti : tmp.length); i++){
+    for (let i = 0; i < (max_noti <= tmp.length ? max_noti : tmp.length); i++) {
         noti_dropdown.appendChild(tmp[i])
     }
 }
@@ -81,7 +108,7 @@ const update_badge = (max_noti) => {
     const badge = document.getElementById("notification-bell");
     if (new_alerts_num == 0) badge.style.display = 'none';
     else {
-        badge.textContent = new_alerts_num < max_noti ? new_alerts_num.toString() : max_noti.toString()+"+";
+        badge.textContent = new_alerts_num < max_noti ? new_alerts_num.toString() : max_noti.toString() + "+";
         badge.style.display = 'block';
     }
 }
@@ -90,14 +117,14 @@ const update_toasts = (data, max_toast) => {
     const toast_container = document.getElementById("toast-container");
     const new_toasts = data.alerts.map(item => toast_child(item));
 
-    if (toast_container.childElementCount >= max_toast){
-        for (let i=0;i<toast_container.childElementCount;i++){
+    if (toast_container.childElementCount >= max_toast) {
+        for (let i = 0; i < toast_container.childElementCount; i++) {
             const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast_container.children[i]);
             toastBootstrap.hide();
         }
     }
 
-    for (let i=0; i < (max_toast <= new_toasts.length ? max_toast : new_toasts.length); i++){
+    for (let i = 0; i < (max_toast <= new_toasts.length ? max_toast : new_toasts.length); i++) {
         toast_container.appendChild(new_toasts[i]);
         const toastBootstrap = bootstrap.Toast.getOrCreateInstance(new_toasts[i]);
         toastBootstrap.show();
@@ -117,6 +144,7 @@ const fetch_notification = () => {
             update_dropdown_list(data, 15);
             update_badge(15);
             update_toasts(data, 4);
+            notify(data);
             document.getElementById('notify-sound').play();
         }
     };
@@ -134,4 +162,7 @@ const fetch_notification = () => {
     };
 };
 
-fetch_notification();
+window.onload = e => {
+    notify_request();
+    fetch_notification();
+}
