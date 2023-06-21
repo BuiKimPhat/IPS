51bec17c93e1e9b5a9db85209ec5c09b14eef3a3
BuiKimPhat <buikimphat42@gmail.com>
2023-06-22 01:33:24 +0700

    UI for rules, iptables rules, rule detail

--- web/monitorsite/monitorweb/urls.py
+++ web/monitorsite/monitorweb/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path("agent/<int:agent_id>/", views.agent_detail, name="agent_detail"),
     path("alerts/", views.alerts, name="alerts"),
     path("logout/", views.logout, name="logout"),
+    path("rules/", views.rules, name="rules"),
+    path("rule/<int:rule_id>/", views.rule_detail, name="rule_detail"),
+    path("iptables_rules/", views.iptables_rules, name="iptables_rules"),
     # APIs
     path("api/mark_alerts/", views.mark_alerts, name="mark_alerts"),
-    path("api/mark_alert/<int:alert_id>/", views.mark_alert, name="mark_alert")
+    path("api/mark_alert/<int:alert_id>/", views.mark_alert, name="mark_alert"),
+    path("api/delete_iptables_rule/", views.delete_iptables_rule, name="delete_iptables_rule")
 ]
